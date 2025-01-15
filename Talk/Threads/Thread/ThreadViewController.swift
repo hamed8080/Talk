34d7a55f3dbdbff43ce68224ed648d1f19ff0309
Hamed Hosseini <hamedhosseini@Senya-MacBook-Pro.local>
2025-01-15 13:55:15 +0300

    [BugFix] Fix a bug after refresh threshold was lower than the id and it led to not load lazy loading in thread list [BugFix] Prevent call scroll to method on TableView if the list is empty or is lower than the indexpath to dodge a crash

--- Talk/Threads/Thread/ThreadViewController.swift
+++ Talk/Threads/Thread/ThreadViewController.swift
@@ -548,6 +548,7 @@ extension ThreadViewController: HistoryScrollDelegate {
     }
 
     func scrollTo(index: IndexPath, position: UITableView.ScrollPosition, animate: Bool = true) {
+        if tableView.numberOfSections == 0 || tableView.numberOfSections < index.section + 1 { return }
         tableView.scrollToRow(at: index, at: position, animated: animate)
     }
 
