f642f8ded3a041b890f0af464a455f0ecde9a138
Hamed Hosseini <hamedhosseini@Senya-MacBook-Pro.local>
2025-01-19 10:26:25 +0300

    [BugFix] Fix a bug if reaction of the thread is disabled you got an error if you clicked on the reaction

--- Talk/Threads/Thread/Views/MessageRows/Cells/Containers/FooterReactions/ReactionCountRowView.swift
+++ Talk/Threads/Thread/Views/MessageRows/Cells/Containers/FooterReactions/ReactionCountRowView.swift
@@ -82,6 +82,7 @@ final class ReactionCountRowView: UIView, UIContextMenuInteractionDelegate {
     }
 
     @objc private func onTapped(_ sender: UIGestureRecognizer) {
+        if viewModel?.threadVM?.thread.reactionStatus == .disable { return }
         if let messageId = viewModel?.message.id, let sticker = row?.sticker {
             viewModel?.threadVM?.reactionViewModel.reaction(sticker, messageId: messageId)
         }
