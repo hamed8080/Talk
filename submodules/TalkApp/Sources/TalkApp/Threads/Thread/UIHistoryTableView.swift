847f9646a66a7678015ec20c13f807563a6875fb
Hamed Hosseini <hamedhosseini@Senya-MacBook-Pro.local>
2025-06-26 18:01:01 +0300

    [BugFix] Now it save scroll position in both user stop deceleration immidiately or by itself over time

--- submodules/TalkApp/Sources/TalkApp/Threads/Thread/UIHistoryTableView.swift
+++ submodules/TalkApp/Sources/TalkApp/Threads/Thread/UIHistoryTableView.swift
@@ -206,15 +206,7 @@ extension UIHistoryTableView {
         Task(priority: .userInitiated) { @DeceleratingActor [weak self] in
             await self?.viewModel?.scrollVM.isEndedDecelerating = true
         }
-
-        Task(priority: .background) { [weak self] in
-            guard let self = self,
-                  let indexPath = indexPathsForVisibleRows?.first,
-                  let threadId = viewModel?.threadId,
-                  let message = viewModel?.historyVM.sectionsHolder.sections[indexPath.section].vms[indexPath.row].message as? Message
-            else { return }
-            await viewModel?.threadsViewModel?.saveScrollPositionVM.saveScrollPosition(threadId: threadId, message: message)
-        }
+        saveScrollPosition()
     }
 
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
@@ -223,6 +215,18 @@ extension UIHistoryTableView {
             Task(priority: .userInitiated) { @DeceleratingActor [weak self] in
                 await self?.viewModel?.scrollVM.isEndedDecelerating = true
             }
+            saveScrollPosition()
+        }
+    }
+    
+    private func saveScrollPosition() {
+        Task(priority: .background) { [weak self] in
+            guard let self = self,
+                  let indexPath = indexPathsForVisibleRows?.first,
+                  let threadId = viewModel?.threadId,
+                  let message = viewModel?.historyVM.sectionsHolder.sections[indexPath.section].vms[indexPath.row].message as? Message
+            else { return }
+            await viewModel?.threadsViewModel?.saveScrollPositionVM.saveScrollPosition(threadId: threadId, message: message)
         }
     }
 }
